f91aaf40cb3fb86f8425a773c7344e1a9739ba59
Anderson Díaz <ingandersondiaz@hotmail.com>
2023-06-11 12:25:02 -0400

    Fixed buttons margin responsive and change api back

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,5 +1,7 @@
 export const Global = {
-  url: "https://taxpeoples.azurewebsites.net/api/",
+  //url: "https://taxpeoples.azurewebsites.net/api/",
+  url: "https://taxpeoples.up.railway.app/api/",
+
   failed:
     '"Ups!!! hay problemas para conectar con la API, revise su conexion a internet"',
 };
